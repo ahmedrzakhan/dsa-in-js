9108154b08330f501ff850162e5d2d0ca037896e
Ahmed Khan <ahmed.khan@smallcase.com>
2023-04-01 21:42:28 +0000

    zero sum subarray

---
+++ algoExpert/arrays/zeroSumSubarray.js
@@ -0,0 +1,40 @@
+/**
+You're given a list of integers nums. Write a function that
+returns a boolean representing whether there exists a zero-sum subarray of
+nums.
+
+A zero-sum subarray is any subarray where all of the values add up to zero.
+A subarray is any contiguous section of the array. For the purposes of this
+problem, a subarray can be as small as one element and as long as the
+original array.
+
+Sample Input
+nums = [-5, -5, 2, 3, -2]
+
+Sample Output
+True // The subarray [-5, 2, 3] has a sum of 0
+
+Optimal Space & Time Complexity
+O(n) time | O(n) space - where n is the length of nums
+ */
+
+/**
+ * O(n) time | O(n) space
+ */
+
+const nums = [-5, -5, 2, -2, -2];
+
+const firstApproach = () => {
+  const sums = new Set();
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+    if (sum === 0 || sums.has(sum)) {
+      return true;
+    }
+    sums.add(sum);
+  }
+  return false;
+};
+
+console.log("firstApproach", firstApproach());
